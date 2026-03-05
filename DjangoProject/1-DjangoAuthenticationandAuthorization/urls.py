d05eb81af820ed579635e2731bf90cb5fab34144
Viktoria-Todorova <viktoria.todorova.1997@gmail.com>
2026-03-05 11:49:00 +0200

    change proj name

--- DjangoProject/1-DjangoAuthenticationandAuthorization/urls.py
+++ DjangoProject/1-DjangoAuthenticationandAuthorization/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for DjangoProject project.
+URL configuration for 1-DjangoAuthenticationandAuthorization project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
